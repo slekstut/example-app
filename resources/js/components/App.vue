be0619373bcbe6f10aa90b4cdc0037de75a0915c
Šarūnas <52723739+slekstut@users.noreply.github.com>
2022-12-06 15:50:44 +0200

    adjust width

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,13 +1,15 @@
 <template>
     <v-app>
-        <v-container max-width="300">
+        <v-container>
+            <v-form>
             <h1>test app</h1>
-            <v-text-field value="U111111" label="Order No. (Read Only)" disabled></v-text-field>
+            <v-text-field class="py-4" value="U111111" label="Order No. (Read Only)" disabled></v-text-field>
             <v-text-field value="U111110" label="Last Order. (Read Only)" disabled></v-text-field>
             <v-date-picker v-model="picker"></v-date-picker>
             <v-text-field label="Truck number"></v-text-field>
             <v-select :items="items" label="Select a client"></v-select>
             <v-file-input show-size counter multiple label="File input"></v-file-input>
+        </v-form>
         </v-container>
     </v-app>
 
@@ -21,3 +23,9 @@ export default {
     }),
 }
 </script>
+
+<style scoped>
+.container {
+    max-width: 500px;
+}
+</style>
