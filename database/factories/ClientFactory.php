054836930823570c1569d156b576d679b11f7f1f
Šarūnas <52723739+slekstut@users.noreply.github.com>
2022-12-07 14:07:29 +0200

    client db seeded

---
+++ database/factories/ClientFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ClientFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'phone' => $this->faker->phoneNumber,
+            'address' => $this->faker->address,
+            'city' => $this->faker->city,
+            'state' => $this->faker->state,
+            'zip' => $this->faker->postcode,
+            'company_name' => $this->faker->company,
+            'company_address' => $this->faker->address,
+
+        ];
+    }
+}
